5454896d26eda79b3474ca42507d8268a9ae1a19
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-09-15 21:55:18 +0200

    upgraded to 54

--- app/config/urls.ts
+++ app/config/urls.ts
@@ -1,5 +1,5 @@
 export const API_URLS = {
-  METEOBLUE_BASE: "https://my.meteoblue.com/packages/basic-1h_basic-day",
+  METEOBLUE_BASE: "https://windchecker.riedmann.rocks/api/meteoblue",
   SPOTS:
     "https://api.riedmann.rocks/windchecker/items/spots?fields=*.*.*&status=published&sort=name",
   SPOT_DETAILS: "https://api.riedmann.rocks/windchecker/items/item",
